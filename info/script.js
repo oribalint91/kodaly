5db2f3fe40b5af58ed0cb1de478c3a0996b5e229
oribalint91 <31454101+oribalint91@users.noreply.github.com>
2026-03-25 12:01:43 +0100

    index update

--- info/script.js
+++ info/script.js
@@ -1,13 +1,42 @@
 document.addEventListener("DOMContentLoaded", function(){
-    const kattinthatoSzo = document.getElementById("word_helyesiras_szerkeszto_toggle");
-    const kep = document.getElementById("word_helyesiras_szerkeszto")
+    
+    const modal = document.getElementById("kep-modal");
+    const modalKep = document.getElementById("modal-benne-levo-kep");
+    const bezaroGomb = document.querySelector(".modal-bezaras");
 
-    kattinthatoSzo.addEventListener("click", function() {
-        if (kep.style.display == "none") {
-            kep.style.display = "block";
-        } else {
-            kep.style.display = "none";
+    const kattinthatoSzavak = document.querySelectorAll(".ia");
+    
+    kattinthatoSzavak.forEach(function(szo) {
+        szo.addEventListener("click", function() {
+            const tipus = this.getAttribute("data-tipus");
+            const celId = this.getAttribute("data-target");
+            const celElem = document.getElementById(celId);
+
+            if (!celElem) return //ha nincs meg a cél, akkor nem fut
+
+            if (tipus == "toggle") {
+                celElem.classList.toggle("lathato"); //a javaslatban "megjelenik" volt a class, de én "lathato"t használok
+            }
+            else if (tipus == "modal") {
+                modalKep.src = celElem.src;
+                modal.style.display = "block";
+            }
+        });
+    });
+
+    //modal bezárása
+    bezaroGomb.addEventListener("click", function(event){
+        modal.style.display = "none";
+    });
+    window.addEventListener("click", function(e) {
+        if (e.target === modal) {
+            modal.style.display = "none";
         }
     });
-}
-)
+    window.addEventListener("keydown", function(e) {
+        if (e.key === "Escape") {
+            modal.style.display = "none";
+        }
+    });
+    
+});
